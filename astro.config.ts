fb842d070a8f0ace7b5ae8922a4baf5717339edd
Marek Kaput <marek.kaput@swmansion.com>
2026-03-02 09:19:18 +0100

    prettier

--- astro.config.ts
+++ astro.config.ts
@@ -1,23 +1,29 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'AI Workbook',
-			logo: {
-				src: './public/favicon.svg',
-			},
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/software-mansion-labs/ai' }],
-			sidebar: [
-				{ label: 'Getting Started', slug: 'getting-started' },
-				{ label: 'Becoming Productive', slug: 'becoming-productive' },
-				{ label: 'Expanding Horizons', slug: 'expanding-horizons' },
-			],
-			components: {
-				Footer: './src/components/Footer.astro',
-			},
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "AI Workbook",
+      logo: {
+        src: "./public/favicon.svg",
+      },
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/software-mansion-labs/ai",
+        },
+      ],
+      sidebar: [
+        { label: "Getting Started", slug: "getting-started" },
+        { label: "Becoming Productive", slug: "becoming-productive" },
+        { label: "Expanding Horizons", slug: "expanding-horizons" },
+      ],
+      components: {
+        Footer: "./src/components/Footer.astro",
+      },
+    }),
+  ],
 });
