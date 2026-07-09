5d0fd5594d647dbb48b7867462162d39d0a802e1
Marek Kaput <marek.kaput@swmansion.com>
2026-07-09 13:33:40 +0200

    add designing loops chapter

--- astro.config.ts
+++ astro.config.ts
@@ -57,6 +57,7 @@ export default defineConfig({
             { slug: "becoming-productive/security" },
             { slug: "becoming-productive/legal-compliance" },
             { slug: "becoming-productive/going-10x" },
+            { slug: "becoming-productive/designing-loops" },
             { slug: "becoming-productive/what-not-to-do" },
           ],
         },
